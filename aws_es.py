98f70af0d1d2da3c732c0ce012b53b7c22365067
kareem <kareem.elsayed@fly365.com>
2019-07-28 23:04:37 +0200

    new testing

--- aws_es.py
+++ aws_es.py
@@ -26,8 +26,6 @@ def get_elastic_indices():
             return stage_url
         elif elastic_url == '3':
             elastic_url = input('Insert your Elasticsearch url : ')
-        if not elastic_url.startswith('https'):
-            return 'https://' + elastic_url
         if not elastic_url.endswith('/'):
             return elastic_url + '/'
         else:
@@ -45,24 +43,33 @@ def list_indices():
     sorted_data = sorted(new_data)
     results = '\n'.join(sorted_data)
     print(results)
+    items = int(input('select range of indices: '))
+    for i in sorted_data[:items]:
+        print(es_url + i)
+        delete_indices = es_url + i
+        requests.delete(delete_indices)
+        print('Deleted...')
+
+
+
 
 # Select which indices do you want to delete
 
 
-def del_indices():
-    while True:
-        insert_indices = input('Select indices [Press any key to exit]: ')
-        if not insert_indices.startswith('logstash'):
-            print('Done')
-            exit()
-        delete_indices = es_url + insert_indices
-        requests.delete(delete_indices)
-        print('Deleted....')
+# def del_indices():
+#     while True:
+#         insert_indices = input('Select indices [Press any key to exit]: ')
+#         if not insert_indices.startswith('logstash'):
+#             print('Done')
+#             exit()
+#         delete_indices = es_url + insert_indices
+#         requests.delete(delete_indices)
+#         print('Deleted....')
 
 
 def main():
     list_indices()
-    del_indices()
+    #del_indices()
 
 
 if __name__ == '__main__':
